4c21aa2028b5cb9738cb27bad035df6bd9b36aab
Christopher Carruthers <christopher971991@gmail.com>
2023-03-17 13:16:04 +1100

    React router pages setup

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.scss";
+
+const Navbar = () => {
+  const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  }, []);
+
+  const currentUser = {
+    id: 1,
+    username: "Chris Carruthers",
+    isSeller: true,
+  };
+
+  return (
+    <div className={active ? "navbar active" : "navbar"}>
+      <div className="container">
+        <div className="logo">
+          {/* <Link to="/"> */}
+          <span className="text">fiverr</span>
+          {/* </Link> */}
+          <span className="dot">.</span>
+        </div>
+        <div className="links">
+          <span>Fiverr Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          {!currentUser?.isSeller && <span>Become a Seller</span>}
+          <span>Sign in</span>
+          {!currentUser && <button>Join</button>}
+          {currentUser && (
+            <div className="user" onClick={() => setOpen(!open)}>
+              <img
+                src="https://img.freepik.com/free-vector/mysterious-mafia-man-smoking-cigarette_52683-34828.jpg?w=1380&t=st=1679009170~exp=1679009770~hmac=91848edf25f87dd4fd39ec5c8ee4771219fbcf8828f98ab62af60c8ccad85a64"
+                alt=""
+              />
+              <span>{currentUser?.username}</span>
+              {open && (
+                <div className="options">
+                  {currentUser?.isSeller && (
+                    <>
+                      <span>Gigs</span>
+                      <span>Add New Gig</span>
+                    </>
+                  )}
+                  <span>Orders</span>
+                  <span>Messages</span>
+                  <span>Logout</span>
+                </div>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+      {active && (
+        <>
+          <hr />
+          <div className="menu">
+            <span>Test</span>
+            <span>Test2</span>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
